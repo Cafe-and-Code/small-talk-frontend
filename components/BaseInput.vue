ecff3b9a066cacc3673febda91bafed4f2754fc0
thanhphuocho <thanhphuocho11@gmail.com>
2023-10-29 13:04:39 +0700

    build-souce

---
+++ components/BaseInput.vue
@@ -0,0 +1,25 @@
+<template>
+  <input type="text" v-model="message" @input="handleInput()" />
+</template>
+<script setup lang="ts">
+import { ref } from "vue";
+const message = ref<string>("");
+interface Props {
+  messageProps: string;
+}
+// onMounted(() => {
+//   message.value = props.message;
+// });
+const props = withDefaults(defineProps<Props>(), {
+  messageProps: "",
+});
+const emits = defineEmits(["input"]);
+const handleInput = () => {
+  emits("input", message.value);
+  // console.log(props.messageProps, message.value);
+};
+watch(message, (val) => {
+  message.value = val;
+  console.log(val);
+});
+</script>
